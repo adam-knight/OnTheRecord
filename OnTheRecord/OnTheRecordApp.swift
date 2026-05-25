aabadcdf177089c302dd65064b9c7da424544035
Adam <adam.elaraby@gmail.com>
2026-05-25 14:07:19 +0100

    Initial Commit

---
+++ OnTheRecord/OnTheRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheRecordApp.swift
+//  OnTheRecord
+//
+//  Created by Adam on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct OnTheRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnTheRecordTests/OnTheRecordTests.swift
@@ -0,0 +1,19 @@
+//
+//  OnTheRecordTests.swift
+//  OnTheRecordTests
+//
+//  Created by Adam on 25/05/2026.
+//
+
+import Testing
+@testable import OnTheRecord
+
+struct OnTheRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
